9b52eb30c4cccf4769291c84e740e06c8f3d8aaa
李光春 <gc@dtapp.net>
2022-09-15 18:12:55 +0800

    - update

--- const.go
+++ const.go
@@ -3,5 +3,5 @@ package eastiot
 const (
 	apiUrl   = "http://m2m.eastiot.net"
 	logTable = "eastiot"
-	Version  = "1.0.6"
+	Version  = "1.0.7"
 )
--- get.go
+++ get.go
@@ -1,9 +1,19 @@
 package eastiot
 
+import "go.dtapp.net/golog"
+
 func (c *Client) GetAppId() string {
 	return c.config.appId
 }
 
 func (c *Client) GetApiKey() string {
 	return c.config.apiKey
+}
+
+func (c *Client) GetLogGorm() *golog.ApiClient {
+	return c.log.logGormClient
+}
+
+func (c *Client) GetLogMongo() *golog.ApiClient {
+	return c.log.logMongoClient
 }
